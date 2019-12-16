e79ce307135ba97b21426b014c46f1ed00c33af9
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-16 00:28:18 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+  class Pokemon
+  attr_accessor :id, :name, :type, :db 
+  
+  def initialize(name:, type:, db:, id:)
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db 
+  end 
+  
+  def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type) 
+    VALUES (?, ?)', name, type)
+    SQL
+    DB.execute(sql,name, type) 
+  end 
+  
+  def self.find(id, db)
+    pokemon = db.execute('SELECT * FROM POKEMON WHERE ID = ?', id)
+    Pokemon.new(id: pokemon[0][0], name: pokemon[0][1], type: pokemon[0][2], db: db)
+  end 
+  
+end
 end
